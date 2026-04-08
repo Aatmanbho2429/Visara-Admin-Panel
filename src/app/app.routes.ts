506133027a0485a831981094576a56a561ad43c6
Aatman Stridely <aatmanbhoraniya12@gmail.com>
2026-04-08 15:15:55 +0530

    file change

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Master } from './views/master/master';
-
-export const routes: Routes = [
-    {path:'',component:Master, loadChildren:()=>import('./views/user/user.routes').then(m=>m.routes)
-    }
-];
